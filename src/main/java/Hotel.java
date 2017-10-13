91f33b9ec7d69aacf7ddd8adbadf4bc970052176
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-10-13 15:27:21 +0400

    First version of project

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -3,7 +3,7 @@ import caches.Cache;
 import java.util.Optional;
 
 public class Hotel {
-    @InjectCache(name="clients")
+    @InjectCache(name = "clients")
     Cache clients;
 
     public Optional<Cache> getClients() {
