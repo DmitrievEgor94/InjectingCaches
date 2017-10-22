4a05446c6464e1860a2d6b81314cecbd0aa9bc32
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-10-22 14:12:40 +0400

    Added packages, changed public modificator to private in MapWithCaches#updateCaches, moved files with data to the same package where caches are, added NotFoundCacheException, added in Injector#inject throwing NotFoundException when needed cache is not found.

--- src/main/java/caches/CacheDeclaration.java
+++
@@ -1,9 +0,0 @@
-package caches;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface CacheDeclaration {
-    String name();
-}
